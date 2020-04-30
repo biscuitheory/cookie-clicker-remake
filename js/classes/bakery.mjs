24b1e256218f65be56e28947c6a4e21756c5a772
Sylvie <sylvie.z.lu@gmail.com>
2020-04-30 10:57:06 +0200

    class bakery buildings started

---
+++ js/classes/bakery.mjs
@@ -0,0 +1,14 @@
+import buildings from './data.mjs';
+
+console.log(buildings)
+export class Bakery {
+    constructor(name, cookies = 0, buildings, cookiesPerClick = 1, cookiesPerSecond = 0){
+        this._name = name
+        this._cookies = cookies
+        this._buildings = buildings
+        this._cookiesPerClick = cookiesPerClick
+        this._cookiesPerSecond = cookiesPerSecond
+    }
+
+
+}
---
+++ js/main.mjs
@@ -0,0 +1,8 @@
+import Bakery from './classes/bakery.mjs';
+console.log(Bakery)
+
+import Building from './classes/building.mjs';
+console.log(Building)
+
+import data from './data.mjs';
+console.log(data)
