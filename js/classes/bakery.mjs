c60978f33c1baff8a2de8d4006374fe8a613171a
Sylvie <sylvie.z.lu@gmail.com>
2020-05-01 20:09:41 +0200

    batiment store

--- js/classes/bakery.mjs
+++ js/classes/bakery.mjs
@@ -1,6 +1,6 @@
 //import {buildings} from 'data';
 export default class Bakery{
-    constructor(name ='le gros cookie', cookies = 5, buildings= [], cookiesPerClick = 1 , cookiesPerSecond = 0 ){
+    constructor(name ='le gros cookie', cookies = 0, buildings= [], cookiesPerClick = 1 , cookiesPerSecond = 0 ){
         this._name = name;
         this._cookies = cookies;
         this._buildings = buildings;
@@ -12,6 +12,10 @@ export default class Bakery{
     }
     get cookies(){
         return this._cookies
+    }
+
+    get cookiesPerSecond(){
+        return this._cookiesPerSecond
     }
 
 }
--- js/main.mjs
+++ js/main.mjs
@@ -4,6 +4,9 @@ console.log(Bakery)
 import Building from '../js/classes/building.mjs';
 console.log(Building)
 
+import { store } from '../js/classes/store.mjs';
+console.log(store)
+
 import { buildings } from "./data.mjs"
 console.log(buildings)
 //information de la boulangerie
@@ -11,7 +14,17 @@ console.log(buildings)
 const myBakery = new Bakery();
 
 
+let titreBakery = document.querySelector('h2')
+titreBakery.innerHTML = myBakery.name
 
+let stockSpan = document.getElementById('cookiesStock').querySelector('span')
+console.log(stockSpan)
+stockSpan.innerHTML = myBakery.cookies
+
+
+let perSecondSpan = document.getElementById('cookiesPerSecond').querySelector('span')
+perSecondSpan.innerHTML = myBakery.cookiesPerSecond
+    
 
 
 
