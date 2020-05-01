b1df8ceb8d576b94a4c4f135b2fb4cd084f14c54
Sylvie <sylvie.z.lu@gmail.com>
2020-05-01 19:44:47 +0200

    tuiles js & css en partie

--- js/main.mjs
+++ js/main.mjs
@@ -4,9 +4,6 @@ console.log(Bakery)
 import Building from '../js/classes/building.mjs';
 console.log(Building)
 
-import { store } from '../js/classes/store.mjs';
-console.log(store)
-
 import { buildings } from './data.mjs'
 console.log(buildings)
 
